8f472069224c4add0067e32ae0f6575c045c4c8c
SeungIl <138782327+NekrosGame@users.noreply.github.com>
2025-07-29 15:28:50 +0900

    (style) 플레이어 주석 추가

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,9 +13,9 @@ public class PlayerController : MonoBehaviour
     private bool IsSliding = false;
 
     [Header("바닥 체크 관련")] // groundLayer에서만 점프 가능
-    public Transform GroundCheck;
-    public float GroundCheckRadius = 0.2f;
-    public LayerMask GroundLayer;
+    public Transform GroundCheck; // 추후 투명도 설정으로 긴 바닥 체크를 구현예정
+    public float GroundCheckRadius = 0.2f; // 해당 범위에 그라운드 레이어가 있을경우만 점프 가능
+    public LayerMask GroundLayer; // 바닥 레이어 
 
     private Rigidbody2D Rb;
     private Animator Animator;
@@ -39,7 +39,7 @@ public class PlayerController : MonoBehaviour
             Jump();
         }
 
-        HandleSlideInput();
+        Slide();
     }
 
     // 플레이어를 오른쪽으로 이동
@@ -56,7 +56,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // 쉬프트를 누르고 있는 경우만 슬라이드 입력을 처리
-    private void HandleSlideInput()
+    private void Slide()
     {
         if (Input.GetKey(KeyCode.LeftShift) && IsGrounded)
         {
@@ -76,6 +76,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    // 추가 메소드
     // 플레이어가 바닥에 닿아있는지 확인
     private void UpdateGroundStatus()
     {
