783588ac05b6cf55ad0bc347ff5c23a5ea2bb516
SeungYeop Oh <sadrain9274@naver.com>
2025-07-29 20:43:54 +0900

    [feat] 씬 변경시 배경음 재생 추가

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-/* 제작 목록
+/* 제작 해야할 목록
 BGM
 배경음악 재생 : BGM 오브젝트
 SFX
@@ -10,7 +11,7 @@ SFX
     1. 점프
     2. 슬라이딩
     3. 장애물 피격
-    4. 추락
+    4. 추락(?)
 UISound
 클릭(선택)
 점수창
@@ -29,6 +30,7 @@ public class SoundManager : MonoBehaviour
         {
             Instance = this;
             DontDestroyOnLoad(Instance);
+            SceneManager.sceneLoaded += OnSceneLoaded; // 씬 이동시 배경음 메서드 추가
         }
         else
         {
