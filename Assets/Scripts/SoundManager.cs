a71c6704b26d01bfdee9f87736688b0321d34afb
SeungYeop Oh <sadrain9274@naver.com>
2025-07-29 19:17:06 +0900

    [feat] 효과음 재생 메서드 추가

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -26,7 +26,22 @@ public class SoundManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
+            DontDestroyOnLoad(Instance);
         }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SFXPlay(string sfxName, AudioClip clip) // 효과음 재생 ("Jump", clip)
+    {
+        GameObject go = new GameObject(sfxName + "Sound"); // 소리를 재생하는 오브젝트 생성
+        AudioSource audioSource = go.AddComponent<AudioSource>(); // 오브젝트에 AudioSource 컴포넌트 추가
+        audioSource.clip = clip;
+        audioSource.Play();
+
+        Destroy(go, clip.length); // 효과음이 끝나면 소리 오브젝트 삭제
     }
 }
 
