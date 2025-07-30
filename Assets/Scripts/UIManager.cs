77f98ecebc79aba25983fbd2611109c5b93023d4
sulg16 <mmopq2@naver.com>
2025-07-30 11:45:17 +0900

    fix

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,33 +7,47 @@ using TMPro;
 public class UIManager : MonoBehaviour
 {
     public static UIManager Instance { get; private set; }
+
+
     public Button quitButton;
 
-
+   
     public TMP_Text scoreText;
 
+    
     [System.Serializable]
+
     public class ButtonEffect
     {
         public Button button;
         public Image targetImage;
         public Sprite newSprite;
     }
-
     public ButtonEffect[] buttonEffects;
     public float revertDelay = 0.5f;
+
+    
+    public GameObject introUI;
+    public GameObject inGameUI;
+    public GameObject gameOverUI;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
 
     private void Start()
     {
         quitButton.onClick.AddListener(QuitGame);
 
-
-
         foreach (var effect in buttonEffects)
         {
             Sprite original = effect.targetImage.sprite;
 
-            // 로컬 변수 복사 필요 (클로저 방지)
+            // 클로저 방지용 로컬 복사
             Button localButton = effect.button;
             Image localImage = effect.targetImage;
             Sprite newSprite = effect.newSprite;
@@ -45,13 +59,6 @@ public class UIManager : MonoBehaviour
             });
         }
     }
-    private void Awake()
-    {
-        if (Instance == null)
-            Instance = this;
-        else
-            Destroy(gameObject);
-    }
 
     public void UpdateScoreUI(int score)
     {
@@ -60,6 +67,7 @@ public class UIManager : MonoBehaviour
             scoreText.text = "점수: " + score.ToString();
         }
     }
+
     IEnumerator RevertAfterDelay(Image image, Sprite originalSprite)
     {
         yield return new WaitForSeconds(revertDelay);
@@ -70,12 +78,30 @@ public class UIManager : MonoBehaviour
     {
         Debug.Log("게임 종료 시도");
 
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;  // 에디터 실행 중단
-#else
-        Application.Quit();  // 빌드된 게임 종료
-#endif
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; //에디터에서 종료 누를 시
+        #else
+        Application.Quit(); //실제 빌드에서 종료 누를시
+        #endif
     }
 
+    // UI 전환 제어
 
+    public void HideAllUI()
+    {
+        introUI.SetActive(false);
+        inGameUI.SetActive(false);
+        gameOverUI.SetActive(false);
+    }
+
+    public void ShowUI(GameObject targetUI)
+    {
+        HideAllUI();
+        targetUI.SetActive(true);
+        targetUI.transform.SetAsLastSibling();
+    }
+
+    public void ShowIntroUI() => ShowUI(introUI);
+    public void ShowInGameUI() => ShowUI(inGameUI);
+    public void ShowGameOverUI() => ShowUI(gameOverUI);
 }
