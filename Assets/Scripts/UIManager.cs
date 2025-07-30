f970f98eab2b003aca343de8b1573d6631acbe68
sulg16 <mmopq2@naver.com>
2025-07-30 11:24:01 +0900

    fix
    수정1

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,9 +1,17 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using TMPro;
+
 
 public class UIManager : MonoBehaviour
 {
+    public static UIManager Instance { get; private set; }
+    public Button quitButton;
+
+
+    public TMP_Text scoreText;
+
     [System.Serializable]
     public class ButtonEffect
     {
@@ -17,11 +25,15 @@ public class UIManager : MonoBehaviour
 
     private void Start()
     {
+        quitButton.onClick.AddListener(QuitGame);
+
+
+
         foreach (var effect in buttonEffects)
         {
             Sprite original = effect.targetImage.sprite;
 
-            // 로컬 변수 복사 필요 (클로저 문제 방지)
+            // 로컬 변수 복사 필요 (클로저 방지)
             Button localButton = effect.button;
             Image localImage = effect.targetImage;
             Sprite newSprite = effect.newSprite;
@@ -33,10 +45,37 @@ public class UIManager : MonoBehaviour
             });
         }
     }
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
 
+    public void UpdateScoreUI(int score)
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = "점수: " + score.ToString();
+        }
+    }
     IEnumerator RevertAfterDelay(Image image, Sprite originalSprite)
     {
         yield return new WaitForSeconds(revertDelay);
         image.sprite = originalSprite;
     }
+
+    public void QuitGame()
+    {
+        Debug.Log("게임 종료 시도");
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;  // 에디터 실행 중단
+#else
+        Application.Quit();  // 빌드된 게임 종료
+#endif
+    }
+
+
 }
